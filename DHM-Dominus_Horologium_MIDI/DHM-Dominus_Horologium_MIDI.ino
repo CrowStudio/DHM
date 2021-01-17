e6d97c28cebcb3eeda9cec3f30439ae447bb5715
CrowStudio <crowstudio@gmail.com>
2021-01-17 10:05:36 +0100

    cleaned up EEPROM read/write functions

--- DHM-Dominus_Horologium_MIDI/DHM-Dominus_Horologium_MIDI.ino
+++ DHM-Dominus_Horologium_MIDI/DHM-Dominus_Horologium_MIDI.ino
@@ -256,21 +256,19 @@ int rotaryReadout() {
   return i;
 }
 
-void EEPROMWriteInt(int p_address, int p_value)
-     {
-     byte lowByte = ((p_value >> 0) & 0xFF);
-     byte highByte = ((p_value >> 8) & 0xFF);
+void EEPROMWriteInt(int p_address, int p_value) {
+  byte lowByte = ((p_value >> 0) & 0xFF);
+  byte highByte = ((p_value >> 8) & 0xFF);
+  
+  EEPROM.write(p_address, lowByte);
+  EEPROM.write(p_address + 1, highByte);
+}
 
-     EEPROM.write(p_address, lowByte);
-     EEPROM.write(p_address + 1, highByte);
-     }
-
-unsigned int EEPROMReadInt(int p_address)
-     {
-     byte lowByte = EEPROM.read(p_address);
-     byte highByte = EEPROM.read(p_address + 1);
-
-     return ((lowByte << 0) & 0xFF) + ((highByte << 8) & 0xFF00);
+unsigned int EEPROMReadInt(int p_address) {
+  byte lowByte = EEPROM.read(p_address);
+  byte highByte = EEPROM.read(p_address + 1);
+  
+  return ((lowByte << 0) & 0xFF) + ((highByte << 8) & 0xFF00);
 }
 
 void detailedTimer() {
